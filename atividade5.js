b0f976b974cac5608ca97755ab3de9e231895201
Rudinei Domingues da Cruz <99224319+rudidomingues@users.noreply.github.com>
2023-10-09 21:51:42 -0300

    Create atividade5.js
    resolvido em aula.

---
+++ atividade5.js
@@ -0,0 +1,11 @@
+//criar duas variaveis contendo dois valores diferentes 
+//Criar uma função de nome soma ao qual vai somar as variaveis anteriores 
+// Printar na tela o resultado 
+var n1 = 2;
+var n2 = 6;
+result = n1 + n2;
+function soma(){
+    console.log (`O resultado da soma é ${result}`);
+
+}
+soma();
