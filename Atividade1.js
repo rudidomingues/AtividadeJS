f6487dbaa683140fb6dba2a93d06a9ba2fa31e5b
Rudinei Domingues da Cruz <99224319+rudidomingues@users.noreply.github.com>
2023-10-09 21:49:07 -0300

    Create Atividade1.js
    Feita em aula.

---
+++ Atividade1.js
@@ -0,0 +1,11 @@
+//Declare uma variavel chamada nome e atribua a ela o seu nome.
+//Declare uma variável chamada idade e atribua a ela a sua idade. 
+//Crie uma função chamada apresentação que exibe uma mensagem utilizando as variáveis nome e idade.
+let nome = "Rudinei";
+let idade = 26;
+function apresentacao(){
+    console.log(`Olá, meu nome é ${nome} e tenho ${idade} anos`);
+}
+// chamando a função para exibição a saudação 
+
+apresentacao();
